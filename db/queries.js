c2ff654d19521a0665fbeeb164504e1a9af015c5
giannifontanot <giannifontanot@gmail.com>
2021-07-29 18:01:49 -0500

    Queries used by the aplication.

--- db/queries.js
+++ db/queries.js
@@ -1,8 +1,38 @@
-let select_role = `SELECT company_db.role.id,
-       company_db.role.title      as Role,
-       company_db.department.name as Department,
-       company_db
-           .role.salary           as Salary
-FROM company_db.role,
-     company_db.department
-where company_db.role.department_id = company_db.department.id`;
+module.exports = {
+    select_role_all: () => {
+        return `SELECT company_db.role.id         as Role_Id,
+                       company_db.role.title      as Role,
+                       company_db.department.name as Department,
+                       company_db
+                           .role.salary           as Salary
+                FROM company_db.role,
+                     company_db.department
+                where company_db.role.department_id = company_db.department.id`;
+    },
+    select_department_all: () => {
+        return `SELECT id, name
+                FROM company_db.department`;
+    },
+    select_employee_all: () => {
+        return `SELECT employeeTable.id                                               as Employee_Id,
+                       CONCAT(employeeTable.first_name, " ", employeeTable.last_name) as Employee,
+                       role.title                                                     as Role,
+                       CONCAT(managerTable.first_name, " ", managerTable.last_name)   as Manager
+                from company_db.employee as employeeTable,
+                     company_db.role,
+                     company_db.employee as managerTable
+                where employeeTable.role_id = role.id
+                  
+                  and managerTable.id = employeeTable.manager_id`;
+    },
+    insert_new_department: (department) => {
+        return `insert into company_db.department (name)
+                values ('${department}');`;
+    },
+    insert_new_role: (title, salary, department_id) => {
+        console.log("insert into company_db.role (title, salary, department_id) values ('"+title+"', "+salary+", "+department_id+");");
+
+        return `insert into company_db.role (title, salary, department_id)
+                values ('${title}',${salary},${department_id});`;
+    },
+};
---
+++ db/queries.sql
@@ -0,0 +1,27 @@
+SELECT company_db.role.id,
+       company_db.role.title      as Role,
+       company_db.department.name as Department,
+       company_db
+           .role.salary           as Salary
+FROM company_db.role,
+     company_db.department
+where company_db.role.department_id = company_db.department.id;
+# -------------------------------------------
+SELECT id, name
+FROM company_db.department;
+# --------------------------------------------
+SELECT employeeTable.id                                               as id,
+       CONCAT(employeeTable.first_name, " ", employeeTable.last_name) as Employee,
+       role.title                                                     as role,
+       CONCAT(managerTable.first_name, " ", managerTable.last_name)   as Manager
+from company_db.employee as employeeTable,
+     company_db.role,
+     company_db.employee as managerTable
+where employeeTable.role_id = role.id
+  and managerTable.id = employeeTable.manager_id;
+# --------------------------------------------
+insert into company_db.department (name)
+values('Production');
+# --------------------------------------------
+insert into company_db.role (title, salary, department_id)
+values('Manufacturing Engineer', 100000,1);
